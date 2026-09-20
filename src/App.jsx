ef974698d9c42257d5242ad0f527632ed3c246f5
Sebastian <sebastian@zoezi.se>
2026-09-20 12:22:18 +0200

    feat: implement recipe fetching from API and initialize recipes state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RecipeList from './components/RecipeList';
 import RecipeForm from './components/RecipeForm';
 
 function App() {
 
-  const [recipes, setRecipes] = useState( [
-    {id: 1, title: "Pannkakor", image: "https://www.recepten.se/bilder/recept/186/main/l/pannkakor.jpg", description: "Enkla och goda"},
-    {id: 2, title: "Köttbullar", image: "https://images.arla.com/recordid/DABB690B-2F08-4D2B-8E47C383C5E1AC30/kottbullar.jpg?width=1200&height=630&mode=crop&format=jpg", description: "Runda"},
-  ]);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function loadRecipes() {
+      const response = await fetch("http://localhost:5148/api/recipes");
+      const data = await response.json();
+      setRecipes(data);
+    }
+    loadRecipes();
+  }, []);
 
   function handleAddRecipe(newRecipe) {
   setRecipes([...recipes, newRecipe]);
